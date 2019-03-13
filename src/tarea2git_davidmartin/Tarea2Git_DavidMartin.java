65e2e0980c1204e905e7174f794cfffdc19f25a0
damaba <xdamabaxhs@yahoo.com>
2019-03-13 10:09:01 +0100

    Crear un objeto jugador

--- src/tarea2git_davidmartin/Tarea2Git_DavidMartin.java
+++ src/tarea2git_davidmartin/Tarea2Git_DavidMartin.java
@@ -15,7 +15,8 @@ public class Tarea2Git_DavidMartin {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Jugador jugador1 = new Jugador("Puyol","100","01");
     }
     
 }
